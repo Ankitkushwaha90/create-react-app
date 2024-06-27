cba1cf4f070f93f4cfc82b1518e72cf9a40c1b60
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2024-06-27 11:49:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import Student from './components/Student';
+
+function App() {
+  return(
+    <div>
+      <Student/>
+    </div>
+  );
+}
+
+export default App;
